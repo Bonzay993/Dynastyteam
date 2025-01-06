5c58a21e5c937b65fd873897a39f5e13c71c1fd4
Ionut Blanaru <blanaru.ionut@gmail.com>
2025-01-06 12:50:53 +0000

    New Json file can be parsed into the news page

---
+++ script.js
@@ -0,0 +1,43 @@
+window.onload=function(){
+    getJsonData();
+}
+
+// Function to fetch and process JSON data
+async function getJsonData() {
+    try {
+        // Fetch the JSON file
+        const response = await fetch('./data.json');
+        const jsonData = await response.json();
+        const newsContainer = document.querySelector('.news-container');
+
+        // Check if the response is successful
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the JSON data
+       
+
+        // Loop through each object in the JSON array
+        jsonData.forEach(item => {
+            // Save each key into a variable
+            const newsDiv = document.createElement('div');
+            newsDiv.classList.add('news');
+
+              // Generate the inner HTML for the news item
+              newsDiv.innerHTML = `
+              <h1>${item.title}</h1>
+              <div class="youtube-embed">
+                  <iframe width="420" height="315" src="${item.youtube}" frameborder="0" allowfullscreen></iframe>
+              </div>
+          `;
+
+          // Append the news div to the news-container
+          newsContainer.appendChild(newsDiv);
+
+      
+        });
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+    }
+}
